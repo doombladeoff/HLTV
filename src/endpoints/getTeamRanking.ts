ff78d3e27e3643a9b7011d0ec4e632c2529b3e34
Zhovtonizhko Dmitriy <47075934+doombladeoff@users.noreply.github.com>
2024-04-17 20:41:45 +0300

    Update getTeamRanking.ts

--- src/endpoints/getTeamRanking.ts
+++ src/endpoints/getTeamRanking.ts
@@ -85,7 +85,6 @@ export const getTeamRanking =
             }
             };
             players[i] = player;
-            //players.push(player);
         }
 
         const changeText = el.find('.change').text()
